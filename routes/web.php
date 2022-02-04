13029d1249a71c52cb12828315dbfbab452cad4f
Cirote <estebanrogel@gmail.com>
2022-02-04 05:32:29 -0500

    Avances disenio de brokers

--- routes/web.php
+++ routes/web.php
@@ -10,4 +10,19 @@ Route::get('/', function ()
 Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () 
 {
     return view('dashboard');
+
 })->name('dashboard');
+
+Route::middleware(['auth:sanctum', 'verified'])
+    ->group(function () 
+    {
+        Route::controller(\App\Http\Controllers\BrokerController::class)
+            ->prefix('broker')
+            ->name('broker.')
+            ->group(function () 
+            {
+                Route::get('/', 'index')->name('index');
+                Route::get('/{id}', 'show')->name('show');
+                Route::post('/', 'store')->name('store');
+            });
+    });
