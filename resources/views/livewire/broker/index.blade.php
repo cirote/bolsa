1437d8d278771012eb966c07c6ddb53fdc4c05f9
Cirote <estebanrogel@gmail.com>
2022-02-12 15:19:14 -0500

    Varios

--- resources/views/livewire/broker/index.blade.php
+++ resources/views/livewire/broker/index.blade.php
@@ -1,22 +1,22 @@
-<x-ui-crud-table :isOpen="$isOpen" :isEditable="$isEditable">
+<x-ui-crud-table :isOpen="$isOpen" :isEditable="$isEditable" >
 
     <x-slot name="header">
         <tr>
-            <x-ui-th sort="asc" style="width: 10%">Sigla</x-ui-th>
-            <x-ui-th sort="desc" style="width: 90px">Nombre</x-ui-th>
-            <th style="width: 50px"></th>
+            <x-ui-th sort="asc">Sigla</x-ui-th>
+            <x-ui-th sort="desc">Nombre</x-ui-th>
+            <x-ui-th >Acciones</x-ui-th>
         <tr>
     </x-slot>
 
     <x-slot name="form">
-        <x-ui-input-text wire:model="model.sigla">Sigla: </x-ui-input-text>
+        <x-ui-input-text wire:model="model.sigla" id="sigla">Sigla: </x-ui-input-text>
         <x-ui-input-text wire:model="model.nombre">Nombre del Broker: </x-ui-input-text>
     </x-slot>
 
     <x-slot name="buttons">
-        <button wire:click="close" type="button" class="btn btn-warning" data-dismiss="modal">@lang('entradas.b_cerrar')</button>
+        <x-ui-button-cancel />
         @if($isEditable)
-            <button wire:click.prevent="store" type="button" class="btn btn-primary">@lang('entradas.b_guardar')</button>
+            <x-ui-button-store />
         @endif
     </x-slot>
 
@@ -24,9 +24,7 @@
     <tr>
         <x-ui-td>{{ $broker->sigla }}</x-ui-td>
         <x-ui-td>{{ $broker->nombre }}</x-ui-td>
-        <x-ui-td>
-            <x-ui-edit-button :id="$broker->id"/>
-        </x-ui-td>
+        <x-ui-td-actions :id="$broker->id"/>
     <tr>
     @endforeach
 
