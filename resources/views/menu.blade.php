a87dc89c85166d95f583114f23c2757def7c1e94
Cirote <estebanrogel@gmail.com>
2022-02-15 19:10:33 -0500

    Menu

---
+++ resources/views/menu.blade.php
@@ -0,0 +1,11 @@
+<x-ui-menu>
+
+    <x-ui-menu-item route='dashboard'>
+        Panel de Control
+    </x-ui-menu-item>
+
+    <x-ui-menu-item route='broker.index'>
+        Broker
+    </x-ui-menu-item>
+
+</x-ui-menu>
