5832c87e917d59ab1fce5ac8781f4ba2e2ab31b2
Cirote <estebanrogel@gmail.com>
2023-01-04 18:31:26 -0500

    Test

--- app/Console/Commands/test.php
+++ app/Console/Commands/test.php
@@ -16,27 +16,101 @@ class test extends Command
     }
 
     public function handle()
-    {
-        // \App\Actions\Importar\ImportarDatosDeIolAction::do();
+    {<x-ui-layout>
 
-        // \App\Actions\ImputarMovimientosOriginalesEnPosicionesAction::do();
+    <x-slot name="header">
+        Panel de control
+    </x-slot>
 
-        // \App\Actions\Mover\MoverIolAStoneXAction::do();
+    <x-ui-box>
 
-        // \App\Actions\Mover\MoverIolAPpiAction::do();
+        <x-slot name="header">
+            Aportes
+        </x-slot>
 
-        // \App\Actions\Mover\MoverStoneXAPpiAction::do();
+        <x-ui-tarjeta>
+            <x-slot name="header">
+                Aportes en dólares
+            </x-slot>
+    
+            {{ number_format($aportes = \App\Actions\Calcular\CalcularAportesEnDolaresAction::do(), 2, ',', '.') }}
+        </x-ui-tarjeta>
 
-        // \App\Actions\Importar\ImportarDatosDePpiAction::do();
+        <x-ui-tarjeta>
+            <x-slot name="header">
+                Retiros en dólares
+            </x-slot>
+    
+            {{ number_format($retiros = \App\Actions\Calcular\CalcularRetirosEnDolaresAction::do(), 2, ',', '.') }}
+        </x-ui-tarjeta>
 
-        // \App\Actions\ImputarMovimientosOriginalesEnPosicionesAction::do();
+        <x-ui-tarjeta>
+            <x-slot name="header">
+                Aportes Netos
+            </x-slot>
+    
+            {{ number_format($aportes - $retiros, 2, ',', '.') }}
+        </x-ui-tarjeta>
 
-        // \App\Actions\Mover\AjustarMetaEnStoneXAction::do();
+        <x-ui-tarjeta>
+            <x-slot name="header">
+                T.I.R.
+            </x-slot>
+    
+            {{ number_format(\App\Actions\Calcular\CalcularTIRActualAction::do() * 100, 2, ',', '.') }} %
+        </x-ui-tarjeta>
 
-        \App\Actions\Importar\ImportarDatosDeStoneXAction::do('transactions-17670277-20220407-083319.csv');
+    </x-ui-box>
 
-        \App\Actions\ImputarMovimientosOriginalesEnPosicionesAction::do();
+    <x-ui-box>
+        
+        <x-slot name="header">
+            Disponibilidades
+        </x-slot>
 
-        return 0;
-    }
+        <x-ui-tarjeta>
+            <x-slot name="header">
+                Saldo de caja en pesos
+            </x-slot>
+    
+            {{ number_format($aportes = \App\Actions\Calcular\CalcularSaldoDeCajaEnPesosAction::do(), 2, ',', '.') }}
+        </x-ui-tarjeta>
+
+        <x-ui-tarjeta>
+            <x-slot name="header">
+                Saldo de caja en dólares
+            </x-slot>
+    
+            {{ number_format($aportes = \App\Actions\Calcular\CalcularSaldoDeCajaEnDolaresAction::do(), 2, ',', '.') }}
+        </x-ui-tarjeta>
+
+    </x-ui-box>
+
+    <x-ui-box>
+        
+        <x-slot name="header">
+            Inversión
+        </x-slot>
+
+        <x-ui-tarjeta>
+            <x-slot name="header">
+                Monto invertido en dólares
+            </x-slot>
+    
+            {{ number_format(\App\Actions\Calcular\CalcularMontoInvertidoEnDolaresAction::do(), 2, ',', '.') }}
+        </x-ui-tarjeta>
+
+        <x-ui-tarjeta>
+            <x-slot name="header">
+                Resultados No Realizados
+            </x-slot>
+    
+            {{ number_format(\App\Actions\Calcular\CalcularResultadoNoRealizadoEnDolaresAction::do(), 2, ',', '.') }}
+        </x-ui-tarjeta>
+
+    </x-ui-box>
+
+    @livewire('movimientos.panel')
+
+</x-ui-layout>}
 }
