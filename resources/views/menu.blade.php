7972daeb2fbb4d9ff5823bbbe6e6f9d8529d7fde
Cirote <estebanrogel@gmail.com>
2023-01-04 21:58:22 -0500

    Menu

--- resources/views/menu.blade.php
+++ resources/views/menu.blade.php
@@ -1,51 +1,47 @@
-<x-ui-menu>
+<x-ui-menu-item route='dashboard'>
+    Panel de Control
+</x-ui-menu-item>
 
-    <x-ui-menu-item route='dashboard'>
-        Panel de Control
-    </x-ui-menu-item>
+<x-ui-menu-item route='recomendaciones'>
+    Recomendaciones
+</x-ui-menu-item>
 
-    <x-ui-menu-item route='recomendaciones'>
-        Recomendaciones
-    </x-ui-menu-item>
+<x-ui-menu-item route='broker.index'>
+    Broker
+</x-ui-menu-item>
 
-    <x-ui-menu-item route='broker.index'>
-        Broker
-    </x-ui-menu-item>
+<x-ui-menu-item route='cuentas.index'>
+    Cuentas
+</x-ui-menu-item>
 
-    <x-ui-menu-item route='cuentas.index'>
-        Cuentas
-    </x-ui-menu-item>
+<x-ui-menu-item route='activos.index'>
+    Activos
+</x-ui-menu-item>
 
-    <x-ui-menu-item route='activos.index'>
-        Activos
-    </x-ui-menu-item>
+<x-ui-menu-item route='movimientos.index'>
+    Movimientos
+</x-ui-menu-item>
 
-    <x-ui-menu-item route='movimientos.index'>
-        Movimientos
-    </x-ui-menu-item>
+<x-ui-menu-item route='posiciones.index'>
+    Posiciones
+</x-ui-menu-item>
 
-    <x-ui-menu-item route='posiciones.index'>
-        Posiciones
-    </x-ui-menu-item>
+<x-ui-menu-item route='posiciones.corto.index'>
+    Posiciones C. Plazo
+</x-ui-menu-item>
 
-    <x-ui-menu-item route='posiciones.corto.index'>
-        Posiciones C. Plazo
-    </x-ui-menu-item>
+<x-ui-menu-item route='globales.index'>
+    Posiciones Globales
+</x-ui-menu-item>
 
-    <x-ui-menu-item route='globales.index'>
-        Posiciones Globales
-    </x-ui-menu-item>
+<x-ui-menu-item route='seguimientos.index'>
+    Seguimientos
+</x-ui-menu-item>
 
-    <x-ui-menu-item route='seguimientos.index'>
-        Seguimientos
-    </x-ui-menu-item>
+<x-ui-menu-item route='estados.index'>
+    Estados
+</x-ui-menu-item>
 
-    <x-ui-menu-item route='estados.index'>
-        Estados
-    </x-ui-menu-item>
-
-    <x-ui-menu-item route='resultados.index'>
-        Resultados
-    </x-ui-menu-item>
-
-</x-ui-menu>
+<x-ui-menu-item route='resultados.index'>
+    Resultados
+</x-ui-menu-item>
