7f8137a21ab1d0bea59cf63607e8e317f9916cde
Esteban <estebanrogel@gmail.com>
2024-08-27 11:47:48 -0300

    Ajustes en la visualización

--- resources/views/livewire/grillas/bandas.blade.php
+++ resources/views/livewire/grillas/bandas.blade.php
@@ -1,10 +1,11 @@
-<x-ui-crud-table title="Bandas de Precios de ({{ $activo->simbolo }}) {{ $activo->denominacion }} {{ $activo->stock ? ' - (' . $activo->stock . ') acciones' : '' }}" :model="$bandas" :mode="$mode">
+<x-ui-crud-table title="Bandas de Precios de ({{ $activo->simbolo }}) {{ $activo->denominacion }} {{ $activo->stock ? ' - Stock actual de ' . $activo->stock . ' acciones' : '' }}" :model="$bandas" :mode="$mode">
 
     <x-slot name="header">
         <tr>
             <x-ui-th sorteable='precio' sortby='{{ $sort_by }}' sortorder='{{ $sort_order }}'>Precio</x-ui-th>
             <x-ui-th sorteable='monto' sortby='{{ $sort_by }}' sortorder='{{ $sort_order }}'>Monto</x-ui-th>
             <x-ui-th sorteable='cantidad' sortby='{{ $sort_by }}' sortorder='{{ $sort_order }}'>Cantidad</x-ui-th>
+            <x-ui-th>Stock teórico</x-ui-th>
             <x-ui-th>Limite inferior</x-ui-th>
             <x-ui-th>Limite superior</x-ui-th>
             <x-ui-th>Estado</x-ui-th>
@@ -20,25 +21,47 @@
         <x-ui-input-number item="model.cantidad">Cantidad: </x-ui-input-number>
     </x-slot>
 
+    @php($stock = 0)
+
     @foreach($bandas as $banda)
-    <x-ui-tr bgcolor=''>
-        <x-ui-td number="{{ $banda->precio }}" />
-        <x-ui-td number="{{ $banda->monto }}" decimals='0'/>
-        <x-ui-td number="{{ $banda->cantidad }}" decimals='0'/>
-        <x-ui-td number="{{ $banda->limite_inferior }}" />
-        <x-ui-td number="{{ $banda->limite_superior }}" />
-        <x-ui-td>{{ $banda->estado ? 'Activa' : '' }}</x-ui-td>
-        <x-ui-td>{{ $banda->precioEnEntorno ? 'Si' : 'No' }}</x-ui-td>
-        <x-ui-td>{{ $banda->grilla->idBandaActual == $banda->id ? "Actual" : '' }}</x-ui-td>
-        <x-ui-td-actions :id="$banda->id">
-            @if (! $banda->estado)
-            <x-ui-button wire:click="activar({{ $banda->id }})">
-                <i class="fa fa-bars"></i>
-                Activar
-            </x-ui-button>
-            @endif
-        </x-ui-td-actions>
-    </x-ui-tr>
+        @php($stock += $banda->cantidad)
+        <x-ui-tr>
+            <x-ui-td align="right">
+                {{ number_format($banda->precio, 2, ',', '.') }} 
+                @if(($banda->grilla->idBandaActual == $banda->id))
+                (
+                    <span class="text-success">Actual {{ number_format($activo->cotizacion, 2, ',', '.') }}</span>
+                )
+                @endif
+            </x-ui-td>
+            <x-ui-td number="{{ $banda->monto }}" decimals='0'/>
+            <x-ui-td number="{{ $banda->cantidad }}" decimals='0'/>
+            <x-ui-td align="right">
+                {{ number_format($stock, 0, ',', '.') }} 
+                @if(($banda->grilla->idBandaActual == $banda->id) && ($activo->stock != $stock))
+                (
+                    @if($activo->stock < $stock)
+                        <span class="text-danger">Faltan {{ number_format($activo->stock - $stock, 0, ',', '.') }} acciones</span>
+                    @else
+                        <span class="text-success">Sobran {{ number_format(abs($stock - $activo->stock), 0, ',', '.') }} acciones</span>
+                    @endif
+                )
+                @endif
+            </x-ui-td>
+            <x-ui-td number="{{ $banda->limite_inferior }}" />
+            <x-ui-td number="{{ $banda->limite_superior }}" />
+            <x-ui-td>{{ $banda->estado ? 'Activa' : '' }}</x-ui-td>
+            <x-ui-td>{{ $banda->precioEnEntorno ? 'Si' : 'No' }}</x-ui-td>
+            <x-ui-td>{{ $banda->grilla->idBandaActual == $banda->id ? "Actual" : '' }}</x-ui-td>
+            <x-ui-td-actions :id="$banda->id">
+                @if (! $banda->estado)
+                <x-ui-button wire:click="activar({{ $banda->id }})">
+                    <i class="fa fa-bars"></i>
+                    Activar
+                </x-ui-button>
+                @endif
+            </x-ui-td-actions>
+        </x-ui-tr>
     @endforeach
 
 </x-ui-crud-table>
