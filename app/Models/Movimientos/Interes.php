5e4612dd1fce2e5ae52fb993e37f73f4c79a0003
Esteban <estebanrogel@gmail.com>
2026-06-09 12:04:58 -0300

    Agrega modelo Interes en Movimientos y Operaciones, implementando la herencia de Parental\HasParent.

---
+++ app/Models/Movimientos/Interes.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Models\Movimientos;
+
+use Parental\HasParent;
+
+class Interes extends Movimiento
+{
+    use HasParent;
+}
---
+++ app/Models/Operaciones/Interes.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Models\Operaciones;
+
+use Parental\HasParent;
+
+class Interes extends Operacion
+{
+    use HasParent;
+}
